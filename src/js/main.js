f36068bc174f70b91de214f0380e6525cca65fae
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-11-07 14:24:41 +0100

    Favourite function

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ const searchButton = document.querySelector(".js-searchButton");
 const searchList = document.querySelector(".js-searchList");
 const image = document.querySelector(".js-image");
 const serieTitle = document.querySelector(".js-serieTitle");
+const favouriteList = document.querySelector(".js-favouritesList");
+
 
 let seriesList = [];
 
@@ -19,21 +21,57 @@ function handleSearch(ev) {
     .then(data => {
         const series = data.data;
         //console.log(Series);
+        searchList.innerHTML = "";
         for (const serie of series) {
-            console.log(serie.title);
-            // if
+        //console.log(serie.title);
+
+            let imageURL = serie.images.jpg.image_url;
+            if (imageURL === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"){
+                imageURL = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+            }
+
             searchList.innerHTML += `
-            <li>
-            <img class="js-image" src="${serie.images.jpg.image_url}" alt="${serie.title}">
+            <li class="js-li" id=${serie.mal_id}>
+            <img class="js-image" src="${imageURL}" alt="${serie.title}">
             <h5 class="js-serieTitle">${serie.title}</h5>
             </li>
             `;
-            
+        seriesList.push(serie);
+
+        const allSeriesDOM = document.querySelectorAll(".js-li");
+        for (const serieDOM of allSeriesDOM){
+            serieDOM.addEventListener("click", handleAddFavourite);
+        }
         }
     })
-}
+    }
 
-searchButton.addEventListener('click', handleSearch);
+    searchButton.addEventListener('click', handleSearch);
+     
+    let favouriteSerieList = [];
+    
+    function handleAddFavourite(event){
+                //console.log("hello");
+            const idCardFavouriteSelected = event.currentTarget.id;
+            const favouriteCardSelected = seriesList.find((serie) => {
+                //console.log(idCardFavouriteSelected);
+                //console.log(serie.mal_id);
+            return serie.mal_id === parseInt(idCardFavouriteSelected);
+        })
+        favouriteSerieList.push(favouriteCardSelected);  
+                //console.log(favouriteSerieList);
+        favouriteList.innerHTML += `
+        <li>
+        <img class="js-fav-image" src="${favouriteCardSelected.images.jpg.image_url}" alt="${favouriteCardSelected.title}">
+        <h5 class="js-fav-serieTitle">${favouriteCardSelected.title}</h5>
+        </li>
+        `;
+    }
+
+
+
+
+
 
 
 
