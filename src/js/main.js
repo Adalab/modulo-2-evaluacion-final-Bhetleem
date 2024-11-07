ea9b5b8217aa0fc2f69462052bb5cbda5716b2ee
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-11-07 10:17:46 +0100

    Add search function

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputText = document.querySelector(".js-input");
+const searchButton = document.querySelector(".js-searchButton");
+const searchList = document.querySelector(".js-searchList");
+
+function getSeries(ev) {
+    ev.preventDefault();
+      //console.log("Ha hecho click");
+      //console.log(inputText.value);
+    fetch(`https://api.jikan.moe/v4/anime?q=${inputText.value}`)
+    .then((response) => response.json())
+    .then(data => {
+        const series = data.data;
+
+       // console.log(series);
+
+    })
+}
+
+
+searchButton.addEventListener('click', getSeries);
+
+
+
+
