1ac9036ff3ca9f1f863c299db93d74eca5eb5b25
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-11-08 13:15:39 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,19 @@
-const o=document.querySelector(".js-input"),u=document.querySelector(".js-searchButton"),r=document.querySelector(".js-searchList");document.querySelector(".js-image");document.querySelector(".js-serieTitle");const d=document.querySelector(".js-favouritesList");let c=[];function m(s){s.preventDefault(),fetch(`https://api.jikan.moe/v4/anime?q=${o.value}`).then(t=>t.json()).then(t=>{const i=t.data;r.innerHTML="";for(const e of i){let n=e.images.jpg.image_url;n==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"&&(n="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"),r.innerHTML+=`
-            <li class="js-li" id=${e.mal_id}>
-            <img class="js-image" src="${n}" alt="${e.title}">
-            <h5 class="js-serieTitle">${e.title}</h5>
+const g=document.querySelector(".js-input"),m=document.querySelector(".js-searchButton"),o=document.querySelector(".js-searchList");document.querySelector(".js-image");document.querySelector(".js-serieTitle");let n=document.querySelector(".js-favouritesList");const f=document.querySelector(".js-resetButton");let a=[];function h(e){e.preventDefault(),fetch(`https://api.jikan.moe/v4/anime?q=${g.value}`).then(r=>r.json()).then(r=>{const s=r.data;o.innerHTML="";for(const t of s){let l=t.images.jpg.image_url;l==="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"&&(l="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"),o.innerHTML+=`
+            <li class="js-li favourite serieCard" id=${t.mal_id}>
+            <img class="js-image" src="${l}" alt="${t.title}">
+            <h5 class="js-serieTitle">${t.title}</h5>
             </li>
-            `,c.push(e);const a=document.querySelectorAll(".js-li");for(const l of a)l.addEventListener("click",f)}})}u.addEventListener("click",m);function f(s){const t=s.currentTarget.id,i=c.find(e=>e.mal_id===parseInt(t));d.innerHTML+=`
-        <li>
-        <img class="js-fav-image" src="${i.images.jpg.image_url}" alt="${i.title}">
-        <h5 class="js-fav-serieTitle">${i.title}</h5>
+            `,a.push(t);const u=document.querySelectorAll(".js-li");for(const d of u)d.addEventListener("click",j)}})}m.addEventListener("click",h);document.querySelectorAll(".favourite");function j(e){const r=e.currentTarget.id,s=a.find(t=>t.mal_id===parseInt(r));console.log(e.currentTarget),e.currentTarget.classList.toggle("selectedcard"),e.currentTarget.classList.toggle("serieCard"),document.querySelector(".js-closeButton"),i.push(s),n.innerHTML+=`
+        <li class="serieCard">
+        <button class="close js-closeButton">X</button>
+        <img class="js-fav-image" src="${s.images.jpg.image_url}" alt="${s.title}">
+        <h5 class="js-fav-serieTitle">${s.title}</h5>
         </li>
-        `}
+        `,localStorage.setItem("favouritesLocalStorage",JSON.stringify(i))}let c=JSON.parse(localStorage.getItem("favouritesLocalStorage")),i=c!==null?c:[];if(i!==null)for(const e of i)console.log(e),n.innerHTML+=`
+        <li class="serieCard">
+        <button class="close js-closeButton">X</button>
+        <img class="js-fav-image" src="${e.images.jpg.image_url}" alt="${e.title}">
+        <h5 class="js-fav-serieTitle">${e.title}</h5>
+        </li>
+        `;function v(){a=[],i=[],localStorage.clear()}f.addEventListener("click",v);
 //# sourceMappingURL=main.js.map
